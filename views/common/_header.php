60407c2d6b64c4a87ecf1bf2cde8706c8e5ddbe8
Andrew Fedyk <fedyk72@gmail.com>
2014-02-11 01:13:32 +0200

    Добавив примітивний responsive в хедер

--- views/common/_header.php
+++ views/common/_header.php
@@ -3,10 +3,10 @@
 <!-- End background -->
 
 <div id="header" class="header">
+  <ul id="header-languages">
+    <li data-lang="en"><a href="#EN">EN</a>
+    <li data-lang="ua"><a href="#UA">UA</a>
+  </ul>
   <a id="header-title" href="/" data-i18n="SPORT AND THE CITY"><?php echo isset($header) ? $header : 'SPORT AND THE CITY' ?></a>
   <p id="header-subtitle" data-i18n="PEOPLE. SOCIETY. IDEOLOGY."><?php echo isset($sub_header) ? $sub_header : 'PEOPLE. SOCIETY. IDEOLOGY.' ?></p>
-  <ul id="header-languages">
-    <li data-lang="en"><a href="#EN">EN</a></li>
-    <li data-lang="ua"><a href="#UA">UA</a></li>
-  </ul>
 </div>
